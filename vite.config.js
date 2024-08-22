2e6030f952ba43cd55287915c4ee221c6c314710
fidelthomet <git@ft0.ch>
2024-08-22 14:51:14 +0200

    remove images from library build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  publicDir: false,
   build: {
     lib: {
       entry: resolve(__dirname, "src/main.js"),
