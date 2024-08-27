dccfc7e6dd23bc76436e276b8d969782896182d5
fidelthomet <git@ft0.ch>
2024-08-27 18:00:36 +0200

    observe src, type, and thresholdSrc attributes

--- src/main.js
+++ src/main.js
@@ -5,47 +5,88 @@ import thresholdMap from "./BlueNoise.png";
 class DitherDither extends HTMLElement {
   constructor() {
     super();
+    this.canvas = undefined;
+    this.media = undefined;
+    this.threshold = undefined;
+    this.initialized = undefined;
+    this.gl = undefined;
+  }
+  static observedAttributes = ["src", "type", "thresholdSrc"];
+  async attributeChangedCallback(name, oldValue, newValue) {
+    if (!this.initialized) return;
+    switch (name) {
+      case "src":
+      case "type":
+      case "thresholdSrc":
+        this.init();
+        break;
+
+      default:
+        break;
+    }
+  }
+  connectedCallback() {
+    const root = this.attachShadow({ mode: "closed" });
+    this.canvas = document.createElement("canvas");
+    this.canvas.style = "display: block; image-rendering: pixelated;";
+    root.appendChild(this.canvas);
     this.init();
   }
   async init() {
+    this.loadMedia = (url, crossOrigin, isVideo) => {
+      return new Promise((resolve) => {
+        const el = isVideo ? document.createElement("video") : new Image();
+        el.src = url;
+        el.crossOrigin = crossOrigin;
+        if (isVideo) {
+          el.playsInline = true;
+          el.muted = true;
+          el.loop = true;
+          el.play();
+          el.addEventListener("playing", () => resolve(el), { once: true });
+        } else {
+          el.addEventListener("load", () => resolve(el), { once: true });
+        }
+      });
+    };
+
     const crossOrigin = this.getAttribute("cross-origin");
-    const shadowMode = this.getAttribute("shadow-mode") ?? "closed";
     const mediaSrc = this.getAttribute("src");
     const isVideo =
       this.getAttribute("type") === "video" ||
-      ["mp4", "webm", "ogg"].includes(mediaSrc.match(/[^.]+$/)[0]);
+      (this.getAttribute("type") == null &&
+        ["mp4", "webm", "ogg"].includes(mediaSrc.match(/[^.]+$/)[0]));
+
     const thresholdSrc = this.getAttribute("threshold-map") ?? thresholdMap;
-    const root = this.attachShadow({ mode: shadowMode });
+    // const root = this.attachShadow({ mode: "closed" });
 
-    const loadingMedia = loadMedia(mediaSrc, crossOrigin, isVideo);
-    const loadingThreshold = loadMedia(thresholdSrc, crossOrigin);
+    const loadingMedia = this.loadMedia(mediaSrc, crossOrigin, isVideo);
+    const loadingThreshold = this.loadMedia(thresholdSrc, crossOrigin);
 
-    const { media, threshold } = await Promise.all([loadingMedia, loadingThreshold]).then(
-      (res) => ({
-        media: res[0],
-        threshold: res[1],
-      })
-    );
+    this.canvas.setAttribute("role", isVideo ? "video" : "image");
+    this.canvas.setAttribute("aria-label", this.getAttribute("alt"));
 
-    const width = media.videoWidth ?? media.width;
-    const height = media.videoHeight ?? media.height;
+    await Promise.all([loadingMedia, loadingThreshold]).then((res) => {
+      this.media = res[0];
+      this.threshold = res[1];
+    });
 
-    const canvas = document.createElement("canvas");
-    canvas.style = "display: block; image-rendering: pixelated;";
-    canvas.width = width;
-    canvas.height = height;
+    const width = this.media.videoWidth ?? this.media.width;
+    const height = this.media.videoHeight ?? this.media.height;
 
-    root.appendChild(canvas);
-    const gl = canvas.getContext("webgl");
+    this.canvas.width = width;
+    this.canvas.height = height;
+
+    const gl = (this.gl = this.canvas.getContext("webgl"));
 
     const program = createProgram(gl, vs, fs);
     gl.useProgram(program);
 
-    const mediaTexture = createTexture(gl, media);
-    const thresholdTexture = createTexture(gl, threshold);
+    this.mediaTexture = createTexture(gl, this.media);
+    this.thresholdTexture = createTexture(gl, this.threshold);
 
-    const positionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    this.positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.positionBuffer);
     gl.bufferData(
       gl.ARRAY_BUFFER,
       new Float32Array([0, 0, width, 0, 0, height, 0, height, width, 0, width, height]),
@@ -73,27 +114,10 @@ class DitherDither extends HTMLElement {
     gl.enableVertexAttribArray(texcoordLocation);
 
     gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height);
-    gl.uniform2f(resolutionThresholdLocation, threshold.width, threshold.height);
+    gl.uniform2f(resolutionThresholdLocation, this.threshold.width, this.threshold.height);
 
     gl.uniform1i(imageLocation, 0);
     gl.uniform1i(thresholdLocation, 1);
-
-    function loadMedia(url, crossOrigin, isVideo) {
-      return new Promise((resolve) => {
-        const el = isVideo ? document.createElement("video") : new Image();
-        el.src = url;
-        el.crossOrigin = crossOrigin;
-        if (isVideo) {
-          el.playsInline = true;
-          el.muted = true;
-          el.loop = true;
-          el.play();
-          el.addEventListener("playing", () => resolve(el), { once: true });
-        } else {
-          el.addEventListener("load", () => resolve(el), { once: true });
-        }
-      });
-    }
 
     function createShader(gl, type, source) {
       const shader = gl.createShader(type);
@@ -142,29 +166,34 @@ class DitherDither extends HTMLElement {
       gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
     }
 
-    function render() {
+    this.render = () => {
       if (isVideo) {
-        updateTexture(gl, mediaTexture, media);
-        requestAnimationFrame(render);
+        updateTexture(gl, this.mediaTexture, this.media);
+        requestAnimationFrame(this.render);
       }
 
       gl.clearColor(0, 0, 0, 0);
       gl.clear(gl.COLOR_BUFFER_BIT);
-
-      gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+      gl.bindBuffer(gl.ARRAY_BUFFER, this.positionBuffer);
       gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
 
       gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
       gl.vertexAttribPointer(texcoordLocation, 2, gl.FLOAT, false, 0, 0);
 
       gl.activeTexture(gl.TEXTURE0);
-      gl.bindTexture(gl.TEXTURE_2D, mediaTexture);
+      gl.bindTexture(gl.TEXTURE_2D, this.mediaTexture);
       gl.activeTexture(gl.TEXTURE1);
-      gl.bindTexture(gl.TEXTURE_2D, thresholdTexture);
+      gl.bindTexture(gl.TEXTURE_2D, this.thresholdTexture);
 
       gl.drawArrays(gl.TRIANGLES, 0, 6);
-    }
-    render();
+
+      if (!isVideo) {
+        // console.log("lose context");
+        // gl.getExtension("WEBGL_lose_context").loseContext();
+      }
+    };
+    this.render();
+    this.initialized = true;
   }
 }
 
