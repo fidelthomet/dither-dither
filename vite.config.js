e48b4c057489c00c1b1a2ab9a8b535bbffd755e7
Fidel Thomet <git@ft0.ch>
2026-09-22 14:10:04 +0200

    upgrade vite; drop umd support

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,9 @@ export default defineConfig({
   publicDir: false,
   build: {
     lib: {
-      entry: resolve(__dirname, "src/main.js"),
-      name: "dither-dither",
+      entry: resolve(import.meta.dirname, "src/main.js"),
       fileName: "dither-dither",
+      formats: ["es"],
     },
   },
 });
